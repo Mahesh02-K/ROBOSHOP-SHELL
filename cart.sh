aaf673c1c377d8ecbf8c3c5b9dfaca3dddfea0a4
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-24 23:18:57 +0530

    roboshop automation

--- cart.sh
+++ cart.sh
@@ -74,7 +74,7 @@ systemctl enable cart &>>$LOG_FILE
 systemctl start cart
 VALIDATE $? "Starting cart"
 
-END_TIME=$(date + %s)
+END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
