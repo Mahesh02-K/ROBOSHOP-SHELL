de9c77358d3f3458a8ace4d4628e96de90fe9475
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-24 23:57:39 +0530

    roboshop automation

--- payment.sh
+++ payment.sh
@@ -6,10 +6,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-$LOGS_FOLDER="/var/log/roboshop-logs"
-$SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-$LOG_FILE="$LOGS_FOLDER/$SCRIPT_DIR.log"
-$SCRIPT_DIR=$PWD
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_DIR.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at : $(date)"
