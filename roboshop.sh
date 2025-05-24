2c440a2d266f8789903b916dc643936a1dc48ada
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-24 21:59:22 +0530

    Roboshop automation

--- roboshop.sh
+++ roboshop.sh
@@ -7,16 +7,16 @@ ZONE_ID="Z01533412ERRNXCSTQSX8" # replace with your ZONE ID
 DOMAIN_NAME="kakuturu.store" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
-for instance in ${INSTANCES[@]}
+for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01e4e081964aa62bb --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
