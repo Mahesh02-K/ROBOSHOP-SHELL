880597fc8d31375d9a95f319278cd9c43dbaf75d
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-24 23:43:50 +0530

    roboshop automation

--- shipping.sh
+++ shipping.sh
@@ -95,7 +95,7 @@ fi
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restarting shipping"
 
-END_TIME=(date +%s)
+END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script execution completed successfully, $Y time taken : $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
