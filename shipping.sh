1cf946974fdf97ffcdb795fca48c8e42139f32e3
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-25 16:57:10 +0530

    roboshop automation

--- shipping.sh
+++ shipping.sh
@@ -99,6 +99,17 @@ else
     echo -e "Database data is $Y already loaded $N, skipping..." | tee -a "$LOG_FILE"
 fi
 
+# mysql -h mysql.kakuturu.store -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+# if [ $? -ne 0 ]
+# then
+#     mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+#     mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+#     mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+#     VALIDATE $? "Loading data into MySQL"
+# else
+#     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
+# fi
+
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
 
